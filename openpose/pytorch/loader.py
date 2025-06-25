5ea0ba43c6719cab72c65437d62ddfa10f1b980d
gengelageTT <gengelage@tenstorrent.com>
2025-06-25 20:23:36 +0000

    add openpose model

---
+++ openpose/pytorch/loader.py
@@ -0,0 +1,55 @@
+# SPDX-FileCopyrightText: (c) 2024 Tenstorrent AI ULC
+#
+# SPDX-License-Identifier: Apache-2.0
+"""
+Openpose model loader implementation
+"""
+import torch
+from diffusers.utils import load_image
+from ...base import ForgeModel
+
+dependencies = ["controlnet_aux==0.0.9"]
+
+
+class ModelLoader(ForgeModel):
+    """Openpose model loader implementation."""
+
+    # Shared configuration parameters
+    model_name = "lllyasviel/ControlNet"
+
+    @classmethod
+    def load_model(cls, dtype_override=None):
+        """Load and return the Openpose model instance with default settings.
+
+        Args:
+            dtype_override: Optional torch.dtype to override the model's default dtype.
+                            If not provided, the model will use its default dtype (typically float32).
+
+        Returns:
+            torch.nn.Module: The Openpose model instance.
+
+        """
+        from controlnet_aux import OpenposeDetector
+
+        model = OpenposeDetector.from_pretrained(cls.model_name)
+
+        if dtype_override is not None:
+            model = model.to(dtype_override)
+
+        return model
+
+    @classmethod
+    def load_inputs(cls):
+        """Load and return sample inputs for the Openpose model with default settings.
+
+        Args:
+            batch_size: Optional batch size to override the default batch size of 1.
+
+        Returns:
+            dict: Input tensors that can be fed to the model.
+        """
+        image = load_image(
+            "https://huggingface.co/lllyasviel/control_v11p_sd15_openpose/resolve/main/images/input.png"
+        )
+        arguments = {"input_image": image, "hand_and_face": True}
+        return arguments
