b6bfdd002f8d9be0849f1d92d711eec018138066
gengelageTT <gengelage@tenstorrent.com>
2025-07-21 21:34:21 +0000

    add d-fine model

--- d_fine/pytorch/loader.py
+++ d_fine/pytorch/loader.py
@@ -2,11 +2,11 @@
 #
 # SPDX-License-Identifier: Apache-2.0
 """
-BEiT model loader implementation for image classification
+D-Fine model loader implementation for object detection
 """
 import torch
-from transformers import BeitImageProcessor, BeitForImageClassification
-from PIL import Image
+from transformers.image_utils import load_image
+from transformers import DFineForObjectDetection, AutoImageProcessor
 from ...tools.utils import get_file
 from typing import Optional
 
@@ -23,27 +23,39 @@ from ...config import (
 
 
 class ModelVariant(StrEnum):
-    """Available BEiT model variants."""
+    """Available D-Fine model variants."""
 
-    BASE = "base"
+    NANO = "nano"
+    SMALL = "small"
+    MEDIUM = "medium"
     LARGE = "large"
+    XLARGE = "xlarge"
 
 
 class ModelLoader(ForgeModel):
-    """BEiT model loader implementation for image classification tasks."""
+    """D-Fine model loader implementation for object detection tasks."""
 
     # Dictionary of available model variants
     _VARIANTS = {
-        ModelVariant.BASE: ModelConfig(
-            pretrained_model_name="microsoft/beit-base-patch16-224",
+        ModelVariant.NANO: ModelConfig(
+            pretrained_model_name="ustc-community/dfine-nano-coco",
+        ),
+        ModelVariant.SMALL: ModelConfig(
+            pretrained_model_name="ustc-community/dfine-small-coco",
+        ),
+        ModelVariant.MEDIUM: ModelConfig(
+            pretrained_model_name="ustc-community/dfine-medium-coco",
         ),
         ModelVariant.LARGE: ModelConfig(
-            pretrained_model_name="microsoft/beit-large-patch16-224",
+            pretrained_model_name="ustc-community/dfine-large-coco",
+        ),
+        ModelVariant.XLARGE: ModelConfig(
+            pretrained_model_name="ustc-community/dfine-xlarge-coco",
         ),
     }
 
     # Default variant to use
-    DEFAULT_VARIANT = ModelVariant.BASE
+    DEFAULT_VARIANT = ModelVariant.SMALL
 
     def __init__(self, variant: Optional[ModelVariant] = None):
         """Initialize ModelLoader with specified variant.
@@ -67,10 +79,10 @@ class ModelLoader(ForgeModel):
             ModelInfo: Information about the model and variant
         """
         return ModelInfo(
-            model="beit",
+            model="d-fine",
             variant=variant,
             group=ModelGroup.GENERALITY,
-            task=ModelTask.CV_IMAGE_CLS,
+            task=ModelTask.CV_OBJECT_DET,
             source=ModelSource.HUGGING_FACE,
             framework=Framework.TORCH,
         )
@@ -83,21 +95,21 @@ class ModelLoader(ForgeModel):
         """
 
         # Initialize processor
-        self.processor = BeitImageProcessor.from_pretrained(
+        self.processor = AutoImageProcessor.from_pretrained(
             self._variant_config.pretrained_model_name
         )
 
         return self.processor
 
     def load_model(self, dtype_override=None):
-        """Load and return the BEiT model instance for this instance's variant.
+        """Load and return the D-Fine model instance for this instance's variant.
 
         Args:
             dtype_override: Optional torch.dtype to override the model's default dtype.
                            If not provided, the model will use its default dtype (typically float32).
 
         Returns:
-            torch.nn.Module: The BEiT model instance for image classification.
+            torch.nn.Module: The D-Fine model instance for object detection.
         """
         # Get the pretrained model name from the instance's variant config
         pretrained_model_name = self._variant_config.pretrained_model_name
@@ -106,33 +118,36 @@ class ModelLoader(ForgeModel):
         if self.processor is None:
             self._load_processor()
 
-        # Load pre-trained model from HuggingFace
-        model = BeitForImageClassification.from_pretrained(pretrained_model_name)
-
+        model_kwargs = {}
         if dtype_override is not None:
-            model = model.to(dtype_override)
+            model_kwargs["torch_dtype"] = dtype_override
+
+        # Load pre-trained model from HuggingFace
+        model = DFineForObjectDetection.from_pretrained(
+            pretrained_model_name, **model_kwargs
+        )
         return model
 
     def load_inputs(self, dtype_override=None, batch_size=1):
-        """Load and return sample inputs for the BEiT model with this instance's variant settings.
+        """Load and return sample inputs for the D-Fine model with this instance's variant settings.
 
         Args:
             dtype_override: Optional torch.dtype to override the model inputs' default dtype.
             batch_size: Optional batch size to override the default batch size of 1.
 
         Returns:
-            dict: Input tensors and attention masks that can be fed to the model.
+            dict: Input tensors (pixel values) that can be fed to the model.
         """
         # Ensure processor is initialized
         if self.processor is None:
             self._load_processor()
 
         image_file = get_file("http://images.cocodataset.org/val2017/000000039769.jpg")
-        image = Image.open(str(image_file))
-        inputs = self.processor(images=image, return_tensors="pt")
+        self.image = load_image(str(image_file))
+        inputs = self.processor(images=self.image, return_tensors="pt")
 
         if dtype_override is not None:
-            inputs["pixel_values"] = inputs["pixel_values"].to(dtype_override)
+            inputs = inputs.to(dtype_override)
 
         # Add batch dimension if batch_size
         for key in inputs:
