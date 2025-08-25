8b0e96dc6d0a72d5e3b9b617542d49bb02a20c15
Kyle Mabee <kmabee@tenstorrent.com>
2025-08-25 19:19:12 +0000

    Expose CARVANA_UNET as unet variant since we run it in tt-torch

--- unet/pytorch/loader.py
+++ unet/pytorch/loader.py
@@ -46,6 +46,9 @@ class ModelVariant(StrEnum):
     # TorchHub brain segmentation UNet
     TORCHHUB_BRAIN_UNET = "torchhub_brain_unet"
 
+    # Carvana UNet (in-repo fallback)
+    CARVANA_UNET = "carvana_unet"
+
 
 class ModelLoader(ForgeModel):
     """UNet model loader implementation supporting multiple sources."""
@@ -65,6 +68,10 @@ class ModelLoader(ForgeModel):
             source=ModelSource.TORCH_HUB,
             hub_repo="mateuszbuda/brain-segmentation-pytorch",
             hub_model="unet",
+        ),
+        ModelVariant.CARVANA_UNET: UnetConfig(
+            pretrained_model_name="carvana_unet",
+            source=ModelSource.CUSTOM,
         ),
     }
 
