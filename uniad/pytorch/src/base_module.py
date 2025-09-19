071106733dead77e2c0b8e74b1109eda0bf378dd
sonalibaskaran2499 <ctr-sbaskaran@ext.tenstorrent.com>
2025-09-19 08:29:27 +0000

    UNIAD pytorch model bringup

---
+++ uniad/pytorch/src/base_module.py
@@ -0,0 +1,68 @@
+# SPDX-FileCopyrightText: (c) 2025 Tenstorrent AI ULC
+#
+# SPDX-License-Identifier: Apache-2.0
+import copy
+import torch.nn as nn
+from abc import ABCMeta
+from typing import Iterable, Optional
+
+
+class BaseModule(nn.Module, metaclass=ABCMeta):
+    """Base module for all modules in openmmlab.
+
+    ``BaseModule`` is a wrapper of ``torch.nn.Module`` with additional
+    functionality of parameter initialization. Compared with
+    ``torch.nn.Module``, ``BaseModule`` mainly adds three attributes.
+
+        - ``init_cfg``: the config to control the initialization.
+        - ``init_weights``: The function of parameter
+            initialization and recording initialization
+            information.
+        - ``_params_init_info``: Used to track the parameter
+            initialization information. This attribute only
+            exists during executing the ``init_weights``.
+
+    Args:
+        init_cfg (dict, optional): Initialization config dict.
+    """
+
+    def __init__(self, init_cfg=None):
+        """Initialize BaseModule, inherited from `torch.nn.Module`"""
+
+        super(BaseModule, self).__init__()
+        self._is_init = False
+
+        self.init_cfg = copy.deepcopy(init_cfg)
+
+    def __repr__(self):
+        s = super().__repr__()
+        if self.init_cfg:
+            s += f"\ninit_cfg={self.init_cfg}"
+        return s
+
+
+class ModuleList(BaseModule, nn.ModuleList):
+    """ModuleList in openmmlab.
+
+    Args:
+        modules (iterable, optional): an iterable of modules to add.
+        init_cfg (dict, optional): Initialization config dict.
+    """
+
+    def __init__(
+        self, modules: Optional[Iterable] = None, init_cfg: Optional[dict] = None
+    ):
+        BaseModule.__init__(self, init_cfg)
+        nn.ModuleList.__init__(self, modules)
+
+
+class Sequential(BaseModule, nn.Sequential):
+    """Sequential module in openmmlab.
+
+    Args:
+        init_cfg (dict, optional): Initialization config dict.
+    """
+
+    def __init__(self, *args, init_cfg=None):
+        BaseModule.__init__(self, init_cfg)
+        nn.Sequential.__init__(self, *args)
