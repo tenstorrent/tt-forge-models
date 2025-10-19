1a2e766864d8ce029451cbdc25080e491d2b2d4a
Aleks Knezevic <aknezevic@tenstorrent.com>
2025-10-19 01:43:21 +0000

    DONT MERGE

--- qwen_2_5/casual_lm/pytorch/loader.py
+++ qwen_2_5/casual_lm/pytorch/loader.py
@@ -202,11 +202,14 @@ class ModelLoader(ForgeModel):
             model_kwargs["torch_dtype"] = dtype_override
 
         model = Qwen2ForCausalLM.from_pretrained(pretrained_model_name, **model_kwargs)
+        model.model.layers = model.model.layers[:1]
         model.eval()
+
+        self.config = model.config
 
         return model
 
-    def load_inputs(self, dtype_override=None, batch_size=1):
+    def load_inputs(self, dtype_override=None, batch_size=2):
         """Load and return sample inputs for the Qwen 2.5 model with this instance's variant settings.
 
         Args:
@@ -243,3 +246,48 @@ class ModelLoader(ForgeModel):
                 inputs[key] = inputs[key].repeat_interleave(batch_size, dim=0)
 
         return inputs
+
+    def get_mesh_config(self, num_devices: int):
+        mesh_shape = (2, num_devices // 2)
+        if self._variant not in [
+            ModelVariant.QWEN_2_5_0_5B,
+            ModelVariant.QWEN_2_5_0_5B_INSTRUCT,
+            ModelVariant.QWEN_2_5_1_5B,
+            ModelVariant.QWEN_2_5_1_5B_INSTRUCT,
+            ModelVariant.QWEN_2_5_3B,
+            ModelVariant.QWEN_2_5_3B_INSTRUCT
+        ]:
+            assert (
+                self.config.num_attention_heads % mesh_shape[1] == 0
+            ), "Attention heads must be divisible by the model axis size"
+        return mesh_shape, ("batch", "model")
+
+    def load_shard_spec(self, model, args, kwargs):
+        if self._variant in [
+            ModelVariant.QWEN_2_5_0_5B,
+            ModelVariant.QWEN_2_5_0_5B_INSTRUCT,
+            ModelVariant.QWEN_2_5_1_5B,
+            ModelVariant.QWEN_2_5_1_5B_INSTRUCT,
+            ModelVariant.QWEN_2_5_3B,
+            ModelVariant.QWEN_2_5_3B_INSTRUCT
+        ]:
+            return None
+
+        shard_specs = {}
+        shard_specs[kwargs["input_ids"]] = ("batch", None)
+        shard_specs[kwargs["attention_mask"]] = ("batch", None)
+        for layer in model.model.layers:
+            shard_specs[layer.mlp.up_proj.weight] = ("model", None)
+            shard_specs[layer.mlp.gate_proj.weight] = ("model", None)
+            shard_specs[layer.mlp.down_proj.weight] = (None, "model")
+
+            shard_specs[layer.self_attn.q_proj.weight] = ("model", None)
+            shard_specs[layer.self_attn.q_proj.bias] = ("model",)
+            shard_specs[layer.self_attn.k_proj.weight] = ("model", None)
+            shard_specs[layer.self_attn.k_proj.bias] = ("model",)
+            shard_specs[layer.self_attn.v_proj.weight] = ("model", None)
+            shard_specs[layer.self_attn.v_proj.bias] = ("model",)
+            shard_specs[layer.self_attn.o_proj.weight] = (None, "model")
+        # shard_specs[model.lm_head.weight] = ("model", None)
+
+        return shard_specs
