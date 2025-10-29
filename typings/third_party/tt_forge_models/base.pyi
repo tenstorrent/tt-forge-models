b33f875dd710dd26e46b1f00a3f08eb3d04bb303
Kyle Mabee <kmabee@tenstorrent.com>
2025-10-29 17:56:10 +0000

    Revert "updates to .pre-commit-config.yaml"
    This reverts commit f43b5fa0da9fb081e8fd1cf44f5702b18cc98974.

--- typings/third_party/tt_forge_models/base.pyi
+++
@@ -1,6 +0,0 @@
-from __future__ import annotations
-from .config import LLMModelConfig
-
-class ForgeModel:
-    _variant_config: LLMModelConfig
-    def __init__(self, variant: str | None = ...) -> None: ...
