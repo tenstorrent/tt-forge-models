b91c2445d3f5c354455393633f85bc7c7145a821
lelanchelian <lelanchelian@users.noreply.github.com>
2026-02-06 11:20:57 +0100

    Modify models hf set3

--- rmbg/pytorch/loader.py
+++ rmbg/pytorch/loader.py
@@ -20,7 +20,7 @@ from ...config import (
     Framework,
     StrEnum,
 )
-from ...tools.utils import get_file
+from datasets import load_dataset
 
 
 class ModelVariant(StrEnum):
@@ -136,8 +136,9 @@ class ModelLoader(ForgeModel):
         if self.transform_image is None:
             self._setup_transforms()
 
-        image_file = get_file("http://images.cocodataset.org/val2017/000000039769.jpg")
-        self.image = Image.open(str(image_file))
+        # Load image from HuggingFace dataset
+        dataset = load_dataset("huggingface/cats-image")["test"]
+        self.image = dataset[0]["image"]
 
         inputs = self.transform_image(self.image).unsqueeze(0)
 
