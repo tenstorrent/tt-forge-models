af98172f52acf484624b30e87aad44293bd8e0f0
gengelageTT <gengelage@tenstorrent.com>
2026-04-02 21:00:34 +0000

    add modified deepseek v3.2 exp

---
+++ deepseek/deepseek_v3_2_exp/modified/pytorch/__init__.py
@@ -0,0 +1,8 @@
+# SPDX-FileCopyrightText: (c) 2025 Tenstorrent AI ULC
+#
+# SPDX-License-Identifier: Apache-2.0
+"""
+DeepSeek V3.2 Exp model implementation modified for Tenstorrent projects.
+"""
+
+from .loader import ModelLoader, ModelVariant
