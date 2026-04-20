002329ae6182b3ed9dacc19bd1da362a02ba7fcc
Nicholas Smith <nsmith@tenstorrent.com>
2026-04-20 21:38:00 +0000

    Add sentence-transformers/bert-base-nli-cls-token embedding loader
    Adds a sentence-transformers BERT-base NLI model that uses CLS-token
    pooling to produce 768-dim sentence embeddings.

---
+++ sentence_transformers/bert_base_nli_cls_token/pytorch/__init__.py
@@ -0,0 +1,7 @@
+# SPDX-FileCopyrightText: (c) 2025 Tenstorrent AI ULC
+#
+# SPDX-License-Identifier: Apache-2.0
+"""
+PyTorch implementation of Sentence-Transformers bert-base-nli-cls-token.
+"""
+from .loader import ModelLoader, ModelVariant
