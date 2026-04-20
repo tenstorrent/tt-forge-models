c62ce89f4f47d5206c44a1608237a141a37d3e09
Nicholas Smith <nsmith@tenstorrent.com>
2026-04-20 17:36:51 +0000

    Add nlpie/tiny-clinicalbert masked language model loader

---
+++ tiny_clinicalbert/masked_lm/pytorch/__init__.py
@@ -0,0 +1,7 @@
+# SPDX-FileCopyrightText: (c) 2026 Tenstorrent AI ULC
+#
+# SPDX-License-Identifier: Apache-2.0
+"""
+TinyClinicalBERT Masked Language Modeling PyTorch implementation.
+"""
+from .loader import ModelLoader, ModelVariant
