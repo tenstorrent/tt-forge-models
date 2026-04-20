2dfd9dfa66dfa4fc2edaafffd46610b05fda1266
Nicholas Smith <nsmith@tenstorrent.com>
2026-04-20 18:57:57 +0000

    Add ddvd233/QoQ-Med-Omni-7B image to text model loader

---
+++ qoq_med_omni_7b/image_to_text/pytorch/__init__.py
@@ -0,0 +1,7 @@
+# SPDX-FileCopyrightText: (c) 2025 Tenstorrent AI ULC
+#
+# SPDX-License-Identifier: Apache-2.0
+"""
+QoQ-Med-Omni-7B image-to-text PyTorch implementation.
+"""
+from .loader import ModelLoader, ModelVariant
