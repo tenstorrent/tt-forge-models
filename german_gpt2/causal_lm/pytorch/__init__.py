fbadb8a48de54ed83a901448f94499f6e2a44adf
Nicholas Smith <nsmith@tenstorrent.com>
2026-04-20 23:50:20 +0000

    Add anonymous-german-nlp/german-gpt2 causal LM loader

---
+++ german_gpt2/causal_lm/pytorch/__init__.py
@@ -0,0 +1,7 @@
+# SPDX-FileCopyrightText: (c) 2025 Tenstorrent AI ULC
+#
+# SPDX-License-Identifier: Apache-2.0
+"""
+German GPT-2 causal language modeling PyTorch implementation.
+"""
+from .loader import ModelLoader
