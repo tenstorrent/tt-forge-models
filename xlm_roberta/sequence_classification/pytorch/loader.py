82b2e17c922950c0bc7d468643fec348a6c20110
Nicholas Smith <nsmith@tenstorrent.com>
2026-04-21 04:20:29 +0000

    Add poltextlab/xlm-roberta-large-execorder-cap-v3 sequence classification variant

--- xlm_roberta/sequence_classification/pytorch/loader.py
+++ xlm_roberta/sequence_classification/pytorch/loader.py
@@ -35,6 +35,7 @@ class ModelVariant(StrEnum):
     XLM_ROBERTA_LARGE_FRENCH_EXECORDER_CAP_V3 = (
         "poltextlab/xlm-roberta-large-french-execorder-cap-v3"
     )
+    XLM_ROBERTA_LARGE_EXECORDER_CAP_V3 = "poltextlab/xlm-roberta-large-execorder-cap-v3"
 
 
 class ModelLoader(ForgeModel):
@@ -59,6 +60,10 @@ class ModelLoader(ForgeModel):
         ),
         ModelVariant.XLM_ROBERTA_LARGE_FRENCH_EXECORDER_CAP_V3: LLMModelConfig(
             pretrained_model_name="poltextlab/xlm-roberta-large-french-execorder-cap-v3",
+            max_length=128,
+        ),
+        ModelVariant.XLM_ROBERTA_LARGE_EXECORDER_CAP_V3: LLMModelConfig(
+            pretrained_model_name="poltextlab/xlm-roberta-large-execorder-cap-v3",
             max_length=128,
         ),
     }
