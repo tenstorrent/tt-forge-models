1d5b1b4ed8d877a0f0d01e44e95e294d3ac50e56
Nicholas Smith <nsmith@tenstorrent.com>
2026-04-21 01:02:42 +0000

    Add attack-vector/SecureModernBERT-NER token classification loader
    Introduces a SecureModernBERT-NER loader (ModernBERT-large fine-tuned on 22
    cybersecurity entity classes for threat intelligence named entity recognition).

---
+++ secure_modernbert_ner/token_classification/pytorch/__init__.py
@@ -0,0 +1,7 @@
+# SPDX-FileCopyrightText: (c) 2026 Tenstorrent AI ULC
+#
+# SPDX-License-Identifier: Apache-2.0
+"""
+SecureModernBERT-NER PyTorch implementation for token classification.
+"""
+from .loader import ModelLoader, ModelVariant
