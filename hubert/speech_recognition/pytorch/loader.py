f943f87e3621436eebc6930281eb806a9f5fbf00
Nicholas Smith <nsmith@tenstorrent.com>
2026-04-21 04:05:59 +0000

    Use explicit HubertForCTC in speech_recognition loader

--- hubert/speech_recognition/pytorch/loader.py
+++ hubert/speech_recognition/pytorch/loader.py
@@ -69,14 +69,14 @@ class ModelLoader(ForgeModel):
         return self._processor
 
     def load_model(self, *, dtype_override=None, **kwargs):
-        from transformers import AutoModelForCTC
+        from transformers import HubertForCTC
 
         model_kwargs = {}
         if dtype_override is not None:
             model_kwargs["torch_dtype"] = dtype_override
         model_kwargs |= kwargs
 
-        model = AutoModelForCTC.from_pretrained(
+        model = HubertForCTC.from_pretrained(
             self._variant_config.pretrained_model_name, **model_kwargs
         )
         model.eval()
