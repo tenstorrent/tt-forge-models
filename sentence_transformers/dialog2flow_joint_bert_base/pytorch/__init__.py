0f6f083d95e2f394e39d007c25fc7209041b5ca2
Nicholas Smith <nsmith@tenstorrent.com>
2026-04-21 02:14:51 +0000

    Add sergioburdisso/dialog2flow-joint-bert-base sentence-transformer loader

---
+++ sentence_transformers/dialog2flow_joint_bert_base/pytorch/__init__.py
@@ -0,0 +1,7 @@
+# SPDX-FileCopyrightText: (c) 2025 Tenstorrent AI ULC
+#
+# SPDX-License-Identifier: Apache-2.0
+"""
+dialog2flow-joint-bert-base PyTorch implementation.
+"""
+from .loader import ModelLoader, ModelVariant
