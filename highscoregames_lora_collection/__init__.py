cd903329912512344cb34c7921b34cf41d78bd34
Nicholas Smith <nsmith@tenstorrent.com>
2026-04-21 02:07:09 +0000

    Add highscoregames12018/lora-collection SDXL LoRA text-to-image loader

---
+++ highscoregames_lora_collection/__init__.py
@@ -0,0 +1,6 @@
+# SPDX-FileCopyrightText: (c) 2026 Tenstorrent AI ULC
+#
+# SPDX-License-Identifier: Apache-2.0
+"""
+highscoregames12018/lora-collection SDXL LoRA text-to-image model (highscoregames12018/lora-collection)
+"""
