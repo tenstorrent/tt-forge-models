8dd1915a4a9b6fad2a67b59390b05166afaf857f
Nicholas Smith <nsmith@tenstorrent.com>
2026-04-21 00:41:59 +0000

    Add poltextlab/HunEmBERT3 variant to bert sequence classification loader

--- bert/sequence_classification/pytorch/loader.py
+++ bert/sequence_classification/pytorch/loader.py
@@ -30,6 +30,7 @@ class ModelVariant(StrEnum):
     SASHA_REGARDV3 = "sasha_RegardV3"
     PHILSCHMID_TINY_BERT_SST2_DISTILLED = "philschmid_Tiny_Bert_Sst2_Distilled"
     AMR_KELEG_NADI2024_BASELINE = "AMR_KELEG_NADI2024_Baseline"
+    POLTEXTLAB_HUNEMBERT3 = "poltextlab_HunEmBERT3"
 
 
 class ModelLoader(ForgeModel):
@@ -65,6 +66,10 @@ class ModelLoader(ForgeModel):
             pretrained_model_name="AMR-KELEG/NADI2024-baseline",
             max_length=128,
         ),
+        ModelVariant.POLTEXTLAB_HUNEMBERT3: LLMModelConfig(
+            pretrained_model_name="poltextlab/HunEmBERT3",
+            max_length=128,
+        ),
     }
 
     # Default variant to use
@@ -84,6 +89,7 @@ class ModelLoader(ForgeModel):
         ModelVariant.SASHA_REGARDV3: "The woman worked as a babysitter.",
         ModelVariant.PHILSCHMID_TINY_BERT_SST2_DISTILLED: "the movie was great!",
         ModelVariant.AMR_KELEG_NADI2024_BASELINE: "مرحبا كيف حالك اليوم",
+        ModelVariant.POLTEXTLAB_HUNEMBERT3: "Nagyon örülök, hogy ma itt lehetek!",
     }
 
     def __init__(self, variant=None):
@@ -122,6 +128,7 @@ class ModelLoader(ForgeModel):
             ModelVariant.SASHA_REGARDV3,
             ModelVariant.PHILSCHMID_TINY_BERT_SST2_DISTILLED,
             ModelVariant.AMR_KELEG_NADI2024_BASELINE,
+            ModelVariant.POLTEXTLAB_HUNEMBERT3,
         ):
             group = ModelGroup.VULCAN
         return ModelInfo(
