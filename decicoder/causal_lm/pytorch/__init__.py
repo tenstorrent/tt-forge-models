4d95545a0a105886aaba41694a4f804ae2dee733
Nicholas Smith <nsmith@tenstorrent.com>
2026-04-21 01:17:27 +0000

    Add Deci/DeciCoder-1b causal LM loader

---
+++ decicoder/causal_lm/pytorch/__init__.py
@@ -0,0 +1,8 @@
+# SPDX-FileCopyrightText: (c) 2025 Tenstorrent AI ULC
+#
+# SPDX-License-Identifier: Apache-2.0
+"""
+DeciCoder PyTorch model implementation for causal language modeling.
+"""
+
+from .loader import ModelLoader, ModelVariant
