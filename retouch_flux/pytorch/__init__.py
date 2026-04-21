52b8e653763f9e05760eb5c53abcfec018167678
Nicholas Smith <nsmith@tenstorrent.com>
2026-04-21 03:17:15 +0000

    Add TDN-M/RetouchFLux LoRA model loader for FLUX.1-dev

--- retouch_flux/pytorch/__init__.py
+++ retouch_flux/pytorch/__init__.py
@@ -2,6 +2,6 @@
 #
 # SPDX-License-Identifier: Apache-2.0
 """
-TDN-M/RetouchFLux PyTorch model implementation for Tenstorrent projects.
+RetouchFLux LoRA PyTorch model implementation for Tenstorrent projects.
 """
 from .loader import ModelLoader, ModelVariant
