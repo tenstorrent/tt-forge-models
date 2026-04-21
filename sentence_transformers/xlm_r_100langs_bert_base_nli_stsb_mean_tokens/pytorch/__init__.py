223472f1c6aacdd7be333b721d4cfd6bdb7783ca
Nicholas Smith <nsmith@tenstorrent.com>
2026-04-21 00:40:54 +0000

    Add sentence-transformers/xlm-r-100langs-bert-base-nli-stsb-mean-tokens multilingual sentence embedding model loader

---
+++ sentence_transformers/xlm_r_100langs_bert_base_nli_stsb_mean_tokens/pytorch/__init__.py
@@ -0,0 +1,7 @@
+# SPDX-FileCopyrightText: (c) 2025 Tenstorrent AI ULC
+#
+# SPDX-License-Identifier: Apache-2.0
+"""
+xlm-r-100langs-bert-base-nli-stsb-mean-tokens PyTorch implementation.
+"""
+from .loader import ModelLoader, ModelVariant
