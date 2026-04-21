d0f2e2f51e3bc36fcb30bc528bcf1323af2b1a53
Nicholas Smith <nsmith@tenstorrent.com>
2026-04-21 02:06:27 +0000

    Add package __init__.py files for llama_3_1_tulu_3_8b

--- llama_3_1_tulu_3_8b/causal_lm/pytorch/__init__.py
+++ llama_3_1_tulu_3_8b/causal_lm/pytorch/__init__.py
@@ -1,5 +1,8 @@
 # SPDX-FileCopyrightText: (c) 2026 Tenstorrent AI ULC
 #
 # SPDX-License-Identifier: Apache-2.0
+"""
+allenai/Llama-3.1-Tulu-3-8B causal language modeling implementation.
+"""
 
 from .loader import ModelLoader, ModelVariant
