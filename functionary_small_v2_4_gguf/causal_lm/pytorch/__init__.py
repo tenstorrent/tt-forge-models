2d85a21ddc011aedf7a59eebc847c9aa7c97ebce
Nicholas Smith <nsmith@tenstorrent.com>
2026-04-21 03:51:17 +0000

    Add meetkai/functionary-small-v2.4-GGUF causal LM model loader

---
+++ functionary_small_v2_4_gguf/causal_lm/pytorch/__init__.py
@@ -0,0 +1,7 @@
+# SPDX-FileCopyrightText: (c) 2025 Tenstorrent AI ULC
+#
+# SPDX-License-Identifier: Apache-2.0
+"""
+Functionary Small v2.4 GGUF PyTorch model implementation for causal language modeling.
+"""
+from .loader import ModelLoader, ModelVariant
