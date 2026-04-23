2046604dc969eccf63f94273ab7a9a81a72ab0ee
Nicholas Smith <nsmith@tenstorrent.com>
2026-04-23 20:42:36 +0000

    Fix mimo/image_to_text/pytorch: set use_cache=False on config post-load for transformers 5.x

--- mimo/image_to_text/pytorch/loader.py
+++ mimo/image_to_text/pytorch/loader.py
@@ -86,7 +86,7 @@ class ModelLoader(ForgeModel):
     def load_model(self, *, dtype_override=None, **kwargs):
         pretrained_model_name = self._variant_config.pretrained_model_name
 
-        model_kwargs = {"low_cpu_mem_usage": True, "use_cache": False}
+        model_kwargs = {"low_cpu_mem_usage": True}
 
         if dtype_override is not None:
             model_kwargs["torch_dtype"] = dtype_override
@@ -97,6 +97,7 @@ class ModelLoader(ForgeModel):
         model = Qwen2_5_VLForConditionalGeneration.from_pretrained(
             pretrained_model_name, **model_kwargs
         )
+        model.config.use_cache = False
         model.eval()
         model = Wrapper(model)
 
