6694a0871fa4f395a0f442f89e716c68bd0332c8
Nicholas Smith <nsmith@tenstorrent.com>
2026-04-24 12:56:11 +0000

    Fix uni_tianyan_v1: remove quantization_config to avoid bitsandbytes dependency

--- uni_tianyan_v1/causal_lm/pytorch/loader.py
+++ uni_tianyan_v1/causal_lm/pytorch/loader.py
@@ -84,10 +84,12 @@ class ModelLoader(ForgeModel):
             model_kwargs["torch_dtype"] = dtype_override
         model_kwargs |= kwargs
 
+        config = AutoConfig.from_pretrained(pretrained_model_name)
         if self.num_layers is not None:
-            config = AutoConfig.from_pretrained(pretrained_model_name)
             config.num_hidden_layers = self.num_layers
-            model_kwargs["config"] = config
+        if hasattr(config, "quantization_config"):
+            delattr(config, "quantization_config")
+        model_kwargs["config"] = config
 
         model = AutoModelForCausalLM.from_pretrained(
             pretrained_model_name, **model_kwargs
