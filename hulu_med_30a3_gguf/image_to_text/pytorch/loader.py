c78f96fbd5b6775c3c9e851c6e735a2151da3179
Nicholas Smith <nsmith@tenstorrent.com>
2026-04-25 10:23:18 +0000

    Fix hulu_med_30a3_gguf: patch get_gguf_hf_weights_map for qwen3_vl_moe

--- hulu_med_30a3_gguf/image_to_text/pytorch/loader.py
+++ hulu_med_30a3_gguf/image_to_text/pytorch/loader.py
@@ -11,6 +11,7 @@ import transformers.models.auto.tokenization_auto as _auto_tokenizer
 import transformers.tokenization_utils_tokenizers as _tok_utils
 from transformers.modeling_gguf_pytorch_utils import (
     load_gguf_checkpoint as _orig_load_gguf_checkpoint,
+    get_gguf_hf_weights_map as _orig_get_gguf_hf_weights_map,
     GGUF_SUPPORTED_ARCHITECTURES,
 )
 from transformers.integrations.ggml import GGUF_TO_FAST_CONVERTERS
@@ -64,11 +65,25 @@ def _patched_load_gguf_checkpoint(gguf_path, return_tensors=False, model_to_load
     return result
 
 
+def _patched_get_gguf_hf_weights_map(
+    hf_model, processor, model_type=None, num_layers=None, qual_name=""
+):
+    if model_type is None:
+        model_type = hf_model.config.model_type
+    # gguf-py uses 'qwen3vlmoe' but transformers uses 'qwen3_vl_moe'
+    if model_type == "qwen3_vl_moe":
+        model_type = "qwen3vlmoe"
+    return _orig_get_gguf_hf_weights_map(
+        hf_model, processor, model_type, num_layers, qual_name
+    )
+
+
 _patch_qwen3vlmoe_support()
 _gguf_utils.load_gguf_checkpoint = _patched_load_gguf_checkpoint
 _config_utils.load_gguf_checkpoint = _patched_load_gguf_checkpoint
 _auto_tokenizer.load_gguf_checkpoint = _patched_load_gguf_checkpoint
 _tok_utils.load_gguf_checkpoint = _patched_load_gguf_checkpoint
+_gguf_utils.get_gguf_hf_weights_map = _patched_get_gguf_hf_weights_map
 
 
 class ModelVariant(StrEnum):
