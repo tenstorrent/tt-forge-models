5049a210b203858418c3bfa6f9c4b50f3a621146
Nicholas Smith <nsmith@tenstorrent.com>
2026-04-25 02:25:05 +0000

    Fix bartowski_thedrummer_valkyrie_49b_v2_1_gguf: load tokenizer from original TheDrummer/Valkyrie-49B-v2.1 repo to avoid 'deci' model type lookup in GGUF metadata

--- bartowski_thedrummer_valkyrie_49b_v2_1_gguf/causal_lm/pytorch/loader.py
+++ bartowski_thedrummer_valkyrie_49b_v2_1_gguf/causal_lm/pytorch/loader.py
@@ -63,13 +63,12 @@ class ModelLoader(ForgeModel):
         )
 
     def _load_tokenizer(self, dtype_override=None):
-        tokenizer_kwargs = {}
+        tokenizer_kwargs = {"trust_remote_code": True}
         if dtype_override is not None:
             tokenizer_kwargs["torch_dtype"] = dtype_override
-        tokenizer_kwargs["gguf_file"] = self.GGUF_FILE
 
         self.tokenizer = AutoTokenizer.from_pretrained(
-            self._variant_config.pretrained_model_name, **tokenizer_kwargs
+            self.ORIGINAL_MODEL_NAME, **tokenizer_kwargs
         )
         if self.tokenizer.pad_token is None:
             self.tokenizer.pad_token = self.tokenizer.eos_token
