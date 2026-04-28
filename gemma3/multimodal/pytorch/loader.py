7bccab96fa552a00f8301f898c29d437dbc311db
Nicholas Smith <nsmith@tenstorrent.com>
2026-04-28 14:31:18 +0000

    fix gemma3 multimodal: correct compressed-tensors ignore paths and load_shard_spec paths for transformers 5.x

--- gemma3/multimodal/pytorch/loader.py
+++ gemma3/multimodal/pytorch/loader.py
@@ -314,7 +314,7 @@ class ModelLoader(ForgeModel):
 
         shard_specs = {}
 
-        for layer in model.vision_tower.vision_model.encoder.layers:
+        for layer in model.model.vision_tower.vision_model.encoder.layers:
             shard_specs[layer.self_attn.q_proj.weight] = ("model", "batch")
             shard_specs[layer.self_attn.k_proj.weight] = ("model", "batch")
             shard_specs[layer.self_attn.v_proj.weight] = ("model", "batch")
@@ -323,7 +323,7 @@ class ModelLoader(ForgeModel):
             shard_specs[layer.mlp.fc1.weight] = ("model", "batch")
             shard_specs[layer.mlp.fc2.weight] = ("batch", "model")
 
-        for layer in model.language_model.layers:
+        for layer in model.model.language_model.layers:
             shard_specs[layer.mlp.up_proj.weight] = ("model", "batch")
             shard_specs[layer.mlp.gate_proj.weight] = ("model", "batch")
             shard_specs[layer.mlp.down_proj.weight] = ("batch", "model")
