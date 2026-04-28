2802525181ed448e606cfffa8bc6e2ee2691f7d9
Nicholas Smith <nsmith@tenstorrent.com>
2026-04-28 08:00:04 +0000

    Fix gemma_3_12b_it_quantized_w8a8: use_fast=False and add compressed-tensors dependency
    transformers 5.x now loads Gemma3ImageProcessor as fast by default;
    use_fast=False restores the slow processor that the checkpoint was saved with.
    compressed-tensors is required for W8A8 compressed-tensors quantization format.

--- gemma_3_12b_it_quantized_w8a8/pytorch/loader.py
+++ gemma_3_12b_it_quantized_w8a8/pytorch/loader.py
@@ -70,7 +70,7 @@ class ModelLoader(ForgeModel):
             kwargs["torch_dtype"] = dtype_override
 
         self.processor = AutoProcessor.from_pretrained(
-            self._variant_config.pretrained_model_name, **kwargs
+            self._variant_config.pretrained_model_name, use_fast=False, **kwargs
         )
 
         return self.processor
