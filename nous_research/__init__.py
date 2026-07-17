21ef5d572ccb1ce786867e2804dbd554751aa60c
Devisetty Mahidhar <ctr-dmahidhar@ext.tenstorrent.com>
2026-07-17 09:04:16 +0530

    Add NousResearch MoE loader for Nous-Hermes-2-Mixtral-8x7B-DPO (#820)
    ### Ticket
    Link to Github Issue
    
    ### Problem description
    Model BringUp Nous-Hermes-2-Mixtral-8x7B-DPO
    
    ### What's changed
    Added loaders.py files for the models
    
    Logs:
    
    [nou_reseach.log](https://github.com/user-attachments/files/30035212/nou_reseach.log)
    
    
    ### Checklist
    - [ ] New/Existing tests provide coverage for changes

--- nous_research/__init__.py
+++
@@ -1,8 +0,0 @@
-# SPDX-FileCopyrightText: (c) 2026 Tenstorrent AI ULC
-#
-# SPDX-License-Identifier: Apache-2.0
-"""
-Nous Hermes 2 Yi 34B model implementation for Tenstorrent projects.
-"""
-# Import from the PyTorch implementation by default
-from .pytorch import ModelLoader
