b166017a3538fce03a251af0111725b3fed89c30
sonalibaskaran2499 <ctr-sbaskaran@ext.tenstorrent.com>
2026-09-24 08:27:05 +0000

    Model Bringup hunyuan_3_moe

---
+++ hunyuan_image_3_moe/pytorch/__init__.py
@@ -0,0 +1,6 @@
+# SPDX-FileCopyrightText: (c) 2026 Tenstorrent AI ULC
+#
+# SPDX-License-Identifier: Apache-2.0
+"""PyTorch loader for HunyuanImage-3.0 (tencent/HunyuanImage-3.0)."""
+
+from .loader import ModelLoader, ModelVariant
